4a6e98223dfe1d9e284efd61ec2af3db7f6a1222
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-16 13:32:14 +0530

    Update EbillTest.java

--- src/test/java/com/ebill/test/EbillTest.java
+++ src/test/java/com/ebill/test/EbillTest.java
@@ -1,12 +1,1 @@
-package com.ebill.test;
-
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
-import com.ebill.service.EbillService;
-
-public class EbillTest {
-  
-    
-}
+***Dummy File***
