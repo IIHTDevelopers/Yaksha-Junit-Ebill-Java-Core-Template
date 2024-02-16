d0b7530b121582dde3c5b249e17ef7dc3da40e92
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-16 10:41:10 +0530

    Update EbillTest.java

--- src/main/java/com/ebill/test/EbillTest.java
+++ src/main/java/com/ebill/test/EbillTest.java
@@ -7,46 +7,6 @@ import org.junit.Test;
 import com.ebill.service.EbillService;
 
 public class EbillTest {
-	// Write JUNIT Test Code
-
-	EbillService bill = null;
-
-	@Test
-	public void testCalculateBillAmount_Input_0() {
-
-		bill = new EbillService(0);
-
-		assertEquals(0, bill.calculateBillAmount(), 0.001);
-	}
-
-	@Test
-	public void testCalculateBillAmount_Input_50() {
-		bill = new EbillService(50);
-		assertEquals(130, bill.calculateBillAmount(), 0.001);
-	}
-
-	@Test
-	public void testCalculateBillAmount_Input_100() {
-		bill = new EbillService(100);
-		assertEquals(292.5, bill.calculateBillAmount(), 0.001);
-	}
-
-	@Test
-	public void testCalculateBillAmount_Input_200() {
-		bill = new EbillService(200);
-		assertEquals(818.5, bill.calculateBillAmount(), 0.001);
-	}
-
-	@Test
-	public void testCalculateBillAmount_Input_1000() {
-		bill = new EbillService(1000);
-		assertEquals(7018.5, bill.calculateBillAmount(), 0.001);
-	}
-
-	@Test
-	public void testCalculateBillAmount_Input_5000() {
-		bill = new EbillService(5000);
-		assertEquals(47018.5, bill.calculateBillAmount(), 0.001);
-	}
+	// Write your JUNIT Test Code here
 
 }
